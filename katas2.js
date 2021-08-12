90fa3f800cac70542c48e10f598c85c5b98b81f4
Aine Mota <motaaine1@gmail.com>
2021-08-12 12:27:30 -0300

    Editei o katas.js com os códigos da atividade.

--- katas2.js
+++ katas2.js
@@ -1,37 +1,62 @@
-// comece a criar a sua função add na linha abaixo
+
+function add(a, b) {
+    let soma = a + b;
+    return soma;
+}
+
+ console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+
+function multiply(x,y){
+    let result = 0;
+    for (let multi = 1; multi <= y; multi++){
+        result = add(result,x);
+    }
+    return result;
+}
+
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
-// comece a criar a sua função multiply na linha abaixo
+
+function power(p,o){
+    let base = 1;
+    for(let exp = 1; exp <= o; exp++){
+        base = multiply(base, p);
+    }
+    return base;
+}
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+ console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
-// comece a criar a sua função power na linha abaixo
+
+function factorial(num){
+    let fat = 1; 
+    for (i = 1;i <= num; i++ ){
+       fat = multiply(i,fat);
+    }
+    return fat;
+}
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
-// comece a criar a sua função factorial na linha abaixo
 
+function fibonacci(num){
+    soma = 0;
+    fibo = 1;
+    antecessor = 0;
 
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+    for (let i = 1; i < num; i++){
+        soma = add(antecessor,fibo);
+        antecessor = fibo;
+        fibo = soma;
+    }
+    return soma;
+}
 
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
+console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
